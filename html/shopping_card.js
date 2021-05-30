53f911684ab489d44021712acae4462b4a18af1d
t3mr0i <detmers1@uni-bremen.de>
2021-05-30 21:49:52 +0200

    backup

--- html/shopping_card.js
+++ html/shopping_card.js
@@ -2,20 +2,37 @@ function product(name, price, amount) {
     this.name = name;
     this.price = price;
     this.amount = amount;
+
     return [this.name, this.price, this.amount];
 }
 
-var pulli_rosa = new product("Rosa Pullover", 59.99, 1);
-var shirt_weiss = new product("Weißes T-Shirt", 29.99, 1);
-var pulli_rot = new product("Roter Pullover", 65.99, 1);
-var jacke_rot = new product("Rote Jacke", 150.00,2);
-var jacke_gelb = new product("Gelbe Jacke", 45.50,2);
+product.add = function() {
+    this.amount +=1;
+    console.log(amount)
+}
+
+
+var pulli_rosa = new product("Rosa Pullover", 59.99,0 );
+var shirt_weiss = new product("Weißes T-Shirt", 29.99,0 );
+var pulli_rot = new product("Roter Pullover", 65.99,0 );
+var jacke_rot = new product("Rote Jacke", 150.00,0);
+var jacke_gelb = new product("Gelbe Jacke", 45.50,0);
 
 var allProducts = [
     pulli_rosa,
     shirt_weiss,
     pulli_rot,
     jacke_rot,
+    jacke_gelb
+]
+
+var cartFall = [
+    pulli_rosa,
+    pulli_rot,
+    jacke_gelb
+]
+
+var cartPeter = [
     jacke_gelb
 ]
 function saveToCart(myItem) {
