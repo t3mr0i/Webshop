3e91ab948562ec98a9d70a7305deadf5fe141960
t3mr0i <detmers1@uni-bremen.de>
2021-05-30 19:06:30 +0200

    added shopping cart with all values and adding them all up to a total. Todo -> Button for increasing/decreasing amount

--- html/shopping_card.js
+++ html/shopping_card.js
@@ -1,24 +1,39 @@
-function product(name, price) {
+function product(name, price, amount) {
     this.name = name;
     this.price = price;
-    return [this.name, this.price];
+    this.amount = amount;
+    return [this.name, this.price, this.amount];
 }
 
-var pulli_rosa = product("Rosa Pullover", 59.99);
-var shirt_weiss = product("Weißes T-Shirt", 29.99);
-var pulli_rot = product("Roter Pullover", 65.99);
-var jacke_rot = product("Rote Jacke", 150.00);
-var jacke_gelb = product("Gelbe Jacke", 45.50);
+var pulli_rosa = new product("Rosa Pullover", 59.99, 1);
+var shirt_weiss = new product("Weißes T-Shirt", 29.99, 1);
+var pulli_rot = new product("Roter Pullover", 65.99, 1);
+var jacke_rot = new product("Rote Jacke", 150.00,2);
+var jacke_gelb = new product("Gelbe Jacke", 45.50,2);
 
-
+var allProducts = [
+    pulli_rosa,
+    shirt_weiss,
+    pulli_rot,
+    jacke_rot,
+    jacke_gelb
+]
 function saveToCart(myItem) {
-
-    var completeShoppingCart = JSON.parse(localStorage.getItem("myShoppingCard"));
+    if (myItem.name === null) {
+        localStorage.setItem(myItem.name, myItem.price);
+    } else {
+        total += Number(myItem.price);
+        localStorage.setItem("total", myItem.price);
+    }
+    let completeShoppingCart = JSON.parse(localStorage.getItem("myShoppingCard")) || [];
     completeShoppingCart.push(myItem);
+
     localStorage.setItem("myShoppingCard", JSON.stringify(completeShoppingCart));
 
 
 }
+let myShoppingCart = [];
+
 console.log(localStorage.getItem("myShoppingCard"));
 
 
