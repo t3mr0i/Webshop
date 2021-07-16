f58978421c32348c5a6bf3c95ee986fd0b7add65
Kai Detmers <detmers1@uni-bremen.de>
2021-07-16 22:41:41 +0200

    submit button

--- php/article.php
+++ php/article.php
@@ -6,10 +6,9 @@ include('setupDB.php');
 	$aname = $_POST['name'];
 	$preis = $_POST['preis'];
 	$beschreibung = $_POST['beschreibung'];
-	$bild = $_POST['bild'];
 	
 
-	$sql = "INSERT INTO artikel (aname, preis, beschreibung, bild) VALUES ('$aname', '$preis', '$beschreibung', '$bild');";
+	$sql = "INSERT INTO artikel (aname, preis, beschreibung) VALUES ('$aname', '$preis', '$beschreibung', );";
 	mysqli_query($con, $sql);
 	
 ?>
